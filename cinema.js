82d6ec45678e341a8a2560a5165ff80ffc6af8db
MATHEUS ALBUQUERQUE <110862229+MatheusFreit@users.noreply.github.com>
2023-03-23 21:07:05 -0300

    Alterando o codigo

--- cinema.js
+++ cinema.js
@@ -1,69 +1,69 @@
 const prompt = require('prompt-sync')({ sigint: true });
 
-let pergunta = [];
 let lista = [];
 
-let pgt = prompt("Você quer comprar ingresso? ")
+let pgt = prompt("Você quer comprar ingresso? ");
 let sim = String(pgt).toLowerCase();
 let nao = String(pgt).toLowerCase();
 
-if(nao==="nao"){
-    console.log("okay, o serviço cinemático agradece!");
-}else if(sim ==="sim"){
-    let op = -1;
-    do {
-        console.log("Escolha a opção tarefa:");
-        console.log("0 - Sair");
-        console.log("1 - Comprar um ingresso!");
-        console.log("2 - Visualizar lista de compras");
-        let opção = prompt("Qual é a opção? ");
-        let op = Number(opção);
-        switch (op){
-            case 0:
-                console.log("Você está saindo do sistema, até logo!!")                
-                break;
-            case 1:
-                ComprarIngresso()              
-                break;                
-            case 2:
-                visualizar()
-               break;        
-            default:
-                console.log("opção invalida, verifique sua opção. Por favor")
-                break;
-        }
-    } while(op!==0);
+if (nao === "nao") {
+  console.log("okay, o serviço cinemático agradece!");
+} else if (sim === "sim") {
+  let op=-1;
+  do {
+    console.log("Escolha a opção tarefa:");
+    console.log("0 - Sair");
+    console.log("1 - Comprar um ingresso!");
+    console.log("2 - Visualizar lista de compras");
+    let opcao = prompt("Qual é a opção? ");
+    op = Number(opcao);
+    switch (op) {
+      case 0:
+        console.log("Você está saindo do sistema, até logo!!");
+        break;
+      case 1:
+        ComprarIngresso();
+        break;
+      case 2:
+        visualizar();
+        break;
+      default:
+        console.log("opção invalida, verifique sua opção. Por favor");
+        break;
+    }
+  } while (op !== 0);
 
-function ComprarIngresso(){
+  function ComprarIngresso() {
     console.log("O seu ingresso vai ser 1-meia ou 2-inteiro?");
     console.log("O ingresso meia está de R$ 6,50");
     console.log("O ingresso inteiro está de R$ 13,00");
-        let valor = prompt("Qual será o ingresso? ")
-        let val = Number(valor);
-        switch (val){
-            case 1:
-                ingressoMeia();
-                break;
-            case 2:
-                ingressoInteiro();
-             break;    
+    let valor = prompt("Qual será o ingresso? ");
+    let val = Number(valor);
+    switch (val) {
+      case 1:
+        ingressoMeia();
+        break;
+      case 2:
+        ingressoInteiro();
+        break;
+      default:
+        console.log("opção de ingresso invalido");
+        break;
+    }
+  }
 
-            default:
-                console.log("opção de ingresso invalido")
-                break;
-        }}
-
-function ingressoMeia(){
-    let  pergunta = prompt("Quantidade de ingresso meia? ");
-    lista.push(`${pergunta} meia`);
-}
-function ingressoInteiro(){
-    let  pergunta = prompt("Quantidade de ingresso interio? ");
-    lista.push(`${pergunta} inteiro`);
-}
-function visualizar(){
+  function ingressoMeia() {
+    let pergunta = prompt("Quantidade de ingresso meia? ");
+    lista.push(pergunta);
+  }
+  function ingressoInteiro() {
+    let pergunta = prompt("Quantidade de ingresso interio? ");
+    lista.push(pergunta);
+  }
+  function visualizar() {
     console.log("-----------Valor final da Compra--------------");
-    lista.forEach((pergunta,indice)=>{console.log(`${indice+1} - ${pergunta}`)})
-}
-   
-}
+    lista.forEach((pergunta, indice) => {
+      console.log(`${indice + 1} - ${pergunta}`);
+    });
+  }
+} 
