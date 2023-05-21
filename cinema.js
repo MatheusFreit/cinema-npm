b3cd05dd1586420bbf2a9661deb5cb9ae9259da2
MATHEUS ALBUQUERQUE <110862229+MatheusFreit@users.noreply.github.com>
2023-05-21 16:11:24 -0300

    Update cinema.js
    Atualizando o código, agora pode somar os valores dos ingressos.

--- cinema.js
+++ cinema.js
@@ -1,75 +1,76 @@
 const prompt = require('prompt-sync')({ sigint: true });
 
 let lista = [];
-let ingresso = [];
+let valorMeia = 6.50;
+let valorInteiro = 13.00;
 
 let pgt = prompt("Você quer comprar ingresso? ");
-let sim = String(pgt).toLowerCase();
-let nao = String(pgt).toLowerCase();
+let sim = pgt.toLowerCase();
+let nao = pgt.toLowerCase();
 
 if (nao === "nao") {
-  console.log("okay, o serviço cinemático agradece!");
+  console.log("Okay, o serviço cinemático agradece!");
 } else if (sim === "sim") {
-  let op=-1;
+  let op = -1;
   do {
-    console.log("Escolha a opção tarefa:");
+    console.log("Escolha a opção:");
     console.log("0 - Sair");
-    console.log("1 - Comprar um ingresso!");
+    console.log("1 - Comprar um ingresso");
     console.log("2 - Visualizar lista de compras");
+    console.log("");
     let opcao = prompt("Qual é a opção? ");
     op = Number(opcao);
     switch (op) {
       case 0:
-        console.log("Você está saindo do sistema, até logo!!");
+        console.log("Você está saindo do sistema. Até logo!");
         break;
       case 1:
-        ComprarIngresso();
+        comprarIngresso();
         break;
       case 2:
         visualizar();
         break;
       default:
-        console.log("opção invalida, verifique sua opção. Por favor");
+        console.log("Opção inválida. Por favor, verifique sua opção.");
         break;
     }
   } while (op !== 0);
 
-
-
-  function ComprarIngresso() {
-    let valorMeia = 6.50;
-    let valorInteiro = 13.00;
-    console.log("O seu ingresso vai ser 1-meia ou 2-inteiro?");
-    console.log("O ingresso meia está de R$ " + valorMeia);
-    console.log("O ingresso inteiro está de R$ "+ valorInteiro);
+  function comprarIngresso() {
+    console.log("O seu ingresso será 1 - meia ou 2 - inteiro?");
+    console.log("");
+    console.log("O ingresso meia está por R$ " + valorMeia.toFixed(2));
+    console.log("O ingresso inteiro está por R$ " + valorInteiro.toFixed(2));
     let valor = prompt("Qual será o ingresso? ");
-    let val = Number(valor);
-    switch (val) {
-      case 1:
-        ingressoMeia();
-        break;
-      case 2:
-        ingressoInteiro();
-        break;
-      default:
-        console.log("opção de ingresso invalido");
-        break;
+    let quantidade = prompt("Quantidade de ingressos? ");
+    if (valor === "1") {
+      lista.push(`${quantidade} ingresso(s) meia`);
+    } else if (valor === "2") {
+      lista.push(`${quantidade} ingresso(s) inteiro`);
+    } else {
+      console.log("Opção de ingresso inválida");
     }
+    console.log("");
   }
 
-  function ingressoMeia() {
-    let pergunta = prompt("Quantidade de ingresso meia? ");
-    if(pergunta == 1 ){lista.push(`${pergunta} ingresso meia`);}else if(pergunta !==1){lista.push(`${pergunta} ingressos meia`);}
-   }
-  function ingressoInteiro() {
-    let pergunta = prompt("Quantidade de ingresso interio? ");
-    if(pergunta ==1){lista.push(`${pergunta} ingresso interio`);}else if(pergunta !==1){lista.push(`${pergunta} ingressos inteiros`);}
-    
-  }
   function visualizar() {
     console.log("-----------Valor final da Compra--------------");
-    lista.forEach((pergunta, indice) => {
-      console.log(`${indice + 1} - ${pergunta}`);
+    console.log("");
+    lista.forEach((ingresso, indice) => {
+      console.log(`${indice + 1} - ${ingresso}`);
     });
+    calcularValorTotal();
   }
-} 
+
+  function calcularValorTotal() {
+    let valorTotal = 0;
+    for (let i = 0; i < lista.length; i++) {
+      let quantidade = Number(lista[i].match(/\d+/)[0]);
+      let tipoIngresso = lista[i].includes("meia") ? valorMeia : valorInteiro;
+      valorTotal += quantidade * tipoIngresso;
+    }
+    console.log("Valor total da compra: R$", valorTotal.toFixed(2));
+  }
+
+  console.log("");
+}
